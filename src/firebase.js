440eec0ae93b6fac78c550a54f25a0421f90c14d
olivecoil <adrian.s.tran@gmail.com>
2017-04-20 12:22:41 -0700

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyBSY9ga4vZ0SZhAHIkD2nwI0w81AMf8ITo",
+  authDomain: "lets-decide-4a05b.firebaseapp.com",
+  databaseURL: "https://lets-decide-4a05b.firebaseio.com",
+  projectId: "lets-decide-4a05b",
+  storageBucket: "lets-decide-4a05b.appspot.com",
+  messagingSenderId: "667939050797" 
+}
+
+firebase.initializeApp(config)
+
+export default firebase
+
+export const database = firebase.database()
+export const auth = firebase.auth()
+export const googleAuthProvider = firebase.auth.GoogleAuthProvider()
+export const facebookAuthProvider = firebase.auth.FacebookAuthProvider()
