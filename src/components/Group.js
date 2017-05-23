2a4631d6991d5524435cc17f8e9eb71e80b56db2
olivecoil <adrian.s.tran@gmail.com>
2017-05-23 15:50:55 -0700

    Added setActive function to the component, when the user clicks an specific event, only the destinations relating to that event is rendered

--- src/components/Group.js
+++ src/components/Group.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { database } from '../firebase'
-import NewEvent from './NewEvent'
 import Events from './Events'
+import '../css/group.css'
 
 class Group extends Component {
   constructor (props) {
@@ -9,10 +9,12 @@ class Group extends Component {
     this.state = {
       name: '',
       members: '',
-      events: ''
+      events: '',
+      isActive: false
     }
     this.groupRef = database.ref(`/groups/${this.props.groupID}/`)
     this.eventsRef = database.ref('/events')
+    this.setActive = this.setActive.bind(this)
   }
 
   componentDidMount () {
@@ -29,21 +31,27 @@ class Group extends Component {
     })     
   }
 
+  setActive () {
+    const { isActive } = this.state
+    this.setState({ isActive: !isActive })
+  }
+
   render () {
     const { handleRemove } = this.props
-    const { name, events } = this.state
+    const { name, events, isActive } = this.state
     return (
       <section>
-        <h4>
+        <a href="#" onClick={this.setActive} className={ isActive ? "group--active" : "group"} >
           {name}
-          <button onClick={handleRemove}>Leave </button>
-        </h4>
-        <NewEvent 
-          group={this.groupRef.key}
-        />
-        <Events 
-          events={events}
-        />
+          <button onClick={handleRemove}>&#9660;</button>
+        </a>
+        { 
+          isActive &&
+          <Events 
+            group={this.groupRef.key}
+            events={events}
+          /> 
+        }
       </section>
     )
   }
