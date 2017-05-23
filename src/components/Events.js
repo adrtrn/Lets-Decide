13016efc9b5db7da751e3bcb0160a58bc72e0cb3
olivecoil <adrian.s.tran@gmail.com>
2017-05-23 15:49:59 -0700

    Added condition to display the New Event component only if the group is clicked. Added styling for events container

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
 import map from 'lodash/map'
 import Event from './Event'
+import NewEvent from './NewEvent'
+import '../css/events.css'
 
 class Events extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      active: ''
+    }
+  }
 
   render () {
-    const { events } = this.props
+    const { events, group } = this.props
     return (
       <div className="events-container">
+      <h3>EVENTS</h3>
+      {
+        group && <NewEvent group={group}/>      
+      }
       {
         map(events, (event, key) => {
           return <Event
